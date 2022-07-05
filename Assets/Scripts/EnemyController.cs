d3b81aa28e7934360137b91974427d340a82cf06
France30 <53886711+France30@users.noreply.github.com>
2022-07-05 15:06:36 +0800

    EnemyController: Remove unnecessary lines

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,15 +5,7 @@ using UnityEngine;
 //Script should assign different values to different enemies(ex. grunt -> tank, with tank taking 3 - 4 shots to kill)
 public class EnemyController : MonoBehaviour
 {
-    //for testing only
-    //will replace this with a field to assign different enemy types
     [SerializeField] private float health;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     private void Update()
